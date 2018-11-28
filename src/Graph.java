47436fbf935d4a4b7f31b285c056de0d4cac3494
NickShermeister <ncsherman159@gmail.com>
2018-11-28 17:45:32 -0500

    pulling

--- src/Graph.java
+++ src/Graph.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Graph {
 	private Helper helper;
 	private int cliqueSize;
-	private int num_teams;
+	private int numTeams;
 	private double[][] adjacency;
 	
 	private int numNodes;
@@ -15,7 +17,7 @@ public class Graph {
 		cliqueSize = 4;
 
 		helper = new Helper();
-		num_teams = (int) java.lang.Math.ceil( (numNodes / cliqueSize));
+		numTeams = (int) java.lang.Math.ceil( (numNodes / cliqueSize));
 
 
 		final double[][] properties = helper.generateProperties(numNodes, numProperties, minAttribute, maxAttribute);
@@ -27,28 +29,83 @@ public class Graph {
 	}
 
 	//Returns a matrix with rows showing different teams with the first column being a score out of 100
-	public int[][] greedycliques(){
-		double[][] editable_adjacency = adjacency;
+	public int[][] greedyCliques(){
+		double[][] editableAdjacency = adjacency;
+		int[] newAdditions = new int[2];
 
-		int[][] final_teams = new int[num_teams][cliqueSize+1];
+		int[][] finalTeams = new int[numTeams][cliqueSize+1]; //+1 to indicate how strong the connections are. 
 
 		//Go through and find the highest edge.
-		int[] loc = highestEdge(editable_adjacency);
+		for (int teamNum = 0; teamNum < numTeams; teamNum++) {
+			for (int memNum = 0; memNum < numTeams; memNum++) {
+				if(memNum == 0) {
+					newAdditions = highestEdge(editableAdjacency, new int[0]);
+					finalTeams[teamNum][memNum] = newAdditions[0];
+					finalTeams[teamNum][1] = newAdditions[1];
+					memNum++;
+				}
+				else {
+					newAdditions = highestEdge(editableAdjacency, Arrays.copyOfRange(finalTeams[teamNum], 0, memNum-1));
+					finalTeams[teamNum][memNum] = newAdditions[0];
+				}
+			}
+		}
+
 
 		//Then repeat for highest sum or edges for all nodes connected to first edge
 
 
-		return final_teams;
+		return finalTeams;
 
 	}
 	
 	
 
-	public int[] highestEdge(double[][] adjacency) {
+	public int[] highestEdge(double[][] adjacency, int[] neighbors) {
 		int[] locs = new int[2];
+		double currHigh = -1;
 		
+
+		if(neighbors.length == 0) {
+			for(int i = 0; i < adjacency.length; i ++) {
+				for (int j = 0; j < adjacency[0].length; j++) {
+					if (adjacency[i][j] > currHigh) {
+						currHigh = adjacency[i][j];
+						locs[0] = i;
+						locs[1] = j;
+					}
+				}
+			}
+		}
+		else {
+			double tempVal;
+			for (int x = 0; x < neighbors.length; x++) { //loop through current members
+				tempVal = 1.0;
+				for (int j = 0; j < adjacency.length; j++) { //Loop through all other people
+					if (searchArray(neighbors, j)) {//j isn't in neighbors{
+						tempVal = tempVal * adjacency[x][j];
+					}
+					
+				}
+				if (tempVal > currHigh) {
+					currHigh = tempVal;
+					locs[0] = x;
+					locs[1] = -1;
+				}
+			}
+		}
 		
 		return locs;
+	}
+	
+	public boolean searchArray(int[] list, int val) {
+		
+		for (int x = 0; x < list.length; x++) {
+			if (list[x] == val) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 
