594f81f0ebdcf795249650c197632a07aa1ae0a2
Nate Sampo <nathan.sampo@students.olin.edu>
2018-11-28 16:40:54 -0500

    moved generate adjacencies and added normalize

--- src/Graph.java
+++ src/Graph.java
@@ -1,20 +1,36 @@
 public class Graph {
 	private Helper helper;
-	private int[][] adjacency;
+	private double[][] adjacency;
 	
 	public Graph() {
 		final int numNodes = 100;
 		final int numProperties = 5;
 		final int minAttribute = 0;
-		final int maxAttribute = 100;
+		final int maxAttribute = 1;
 		
 		helper = new Helper();
 		
-		final int[][] properties = helper.generateProperties(numNodes, numProperties, minAttribute, maxAttribute);
+		final double[][] properties = helper.generateProperties(numNodes, numProperties, minAttribute, maxAttribute);
 		
-		adjacency = helper.generateAdjacency(properties);
+		adjacency = generateAdjacency(properties);
+		adjacency = helper.normalize(adjacency);
 		
 		helper.arrayPrint(adjacency);
+	}
+	
+	public double[][] generateAdjacency(double[][] properties) {
+		double[][] adjacencyArray = new double[properties.length][properties.length];
+		
+		for(int i=0; i<properties.length; i++) {
+			for(int j=0; j<properties.length; j++) {
+				for(int k=0; k<properties[0].length; k++) {
+					// Currently sums difference of attributes. Add real weighting here
+					adjacencyArray[i][j] += Math.abs(properties[i][k] - properties[j][k]);
+				}
+			}
+		}
+		
+		return adjacencyArray;
 	}
 	
 	public static void main(String args[]) {
