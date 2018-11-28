61d559709a5bad4f953c984d839c3a6846fcc0ac
Kyle Combes <kylecombes@gmail.com>
2018-11-28 16:37:56 -0500

    Add person profile and first pass edge weighting.

--- src/Helper.java
+++ src/Helper.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Helper {
@@ -12,7 +13,7 @@ public class Helper {
 				}
 			}
 		}
-		
+
 		return adjacency;
 	}
 	
@@ -28,6 +29,50 @@ public class Helper {
 		
 		return propertiesArray;
 	}
+
+	private Profile generateRandomProfile(int id, int numSkills, int maxSilverBullets, int totalNumPeople) {
+		Profile prof = new Profile(id, numSkills);
+
+		// Generate some random skill levels
+		int skillsMin = 0;
+		int skillsMax = 5;
+		for (int skillIdx = 0; skillIdx < numSkills; ++skillIdx) {
+			prof.skills[skillIdx] = ThreadLocalRandom.current().nextInt(skillsMin, skillsMax+1);
+		}
+
+		// Pick some silver bullets
+		int numSilverBullets = ThreadLocalRandom.current().nextInt(0, maxSilverBullets+1);
+		for (int i = 0; i < numSilverBullets; ++i) {
+			int silverBulletedPersonId = ThreadLocalRandom.current().nextInt(0, totalNumPeople);
+			prof.silverBullets.add(silverBulletedPersonId);
+		}
+
+		return prof;
+	}
+
+	/**
+	 * Calculates the edge weight from p1 to p2.
+	 * @param p1 the profile of the starting node
+	 * @param p2 the profile of the ending node
+	 * @param skillsWeight how to weight the lack of skills overlap
+	 * @param preferenceWeight how to weight the preference overlap
+	 * @return the weight of the edge from p1 to p2, where a lower score is more desirable
+	 */
+	 public double calculateEdgeWeight(Profile p1, Profile p2, double skillsWeight, double preferenceWeight) {
+		return skillsWeight * dotProduct(p1.skills, p2.skills)
+				+ preferenceWeight * (p1.preferredPartners.contains(p2.id) ? 0 : 1);
+	}
+
+	/**
+	 * Calculates the dot product of two vectors.
+	 */
+	private double dotProduct(double[] v1, double[] v2) {
+		double res = 0.0;
+		for (int i = 0; i < v1.length; ++i) {
+			res += v1[i] * v2[i];
+		}
+		return res;
+	}
 	
 	public void arrayPrint(int[][] array) {
 		// Iterate through the 2D array and print every value in a way that looks nice
@@ -36,6 +81,20 @@ public class Helper {
 				System.out.print(" " + array[i][j]);
 			}
 			System.out.println("");
+		}
+	}
+
+	public class Profile {
+		public int id;
+		public double[] skills;
+		public HashSet<Integer> preferredPartners;
+		public HashSet<Integer> silverBullets;
+
+		public Profile(int id, int numSkills) {
+			this.id = id;
+			skills = new double[numSkills];
+			preferredPartners = new HashSet<>();
+			silverBullets = new HashSet<>();
 		}
 	}
 }
