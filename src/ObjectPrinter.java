f014a0ad29672b2987d9538d723255662d808e22
Kyle Combes <kylecombes@gmail.com>
2018-12-12 19:57:18 -0500

    Start sweeping weights.

--- src/ObjectPrinter.java
+++ src/ObjectPrinter.java
@@ -1,12 +1,11 @@
 import java.lang.StringBuilder;
+import java.util.List;
 
 public class ObjectPrinter {
 
     private static final int DOUBLE_DECIMAL_PLACES = 2;
 
     public static void printTeamSetScore(TeamSetScore score) {
-        printTeamArray(score.teams);
-
         // Print out the summary statistics
         StringBuilder sb = new StringBuilder();
         sb.append(score.allTeamsValid ? "All teams valid" : "Some teams invalid");
@@ -28,7 +27,31 @@ public class ObjectPrinter {
         System.out.println(sb.toString());
     }
 
+    public static void printTeamSetScoreList(List<TeamSetScore> scores) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Valid?\tTot skill min\tTot skill max\tTot skill range\tPts by skill total SD\tTotal SD\tPart prefs met\tCommon proj\n");
+        for (TeamSetScore score : scores) {
+            sb.append(score.allTeamsValid ? "1" : "0");
+            sb.append('\t');
+            appendDouble(sb, score.totalSkillMin);
+            sb.append('\t');
+            appendDouble(sb, score.totalSkillMax);
+            sb.append('\t');
+            appendDouble(sb, score.totalSkillMax - score.totalSkillMin);
+            sb.append('\t');
+            appendArray(sb, score.pointsBySkillSD);
+            sb.append('\t');
+            appendDouble(sb, score.pointsSD);
+            sb.append('\t');
+            appendDouble(sb, score.meanPartPrefsSatisfied);
+
+            sb.append('\n');
+        }
+        System.out.println(sb.toString());
+    }
+
     public static void printTeamArray(Team[] teams) {
+        System.out.println("Valid?\tTotal pts\tSkill total\tSkill range\tSkill mean\tSkill SD\tMean pts\tPart prefs\tCommon proj");
         for (Team t : teams) {
             printTeam(t);
         }
@@ -43,47 +66,51 @@ public class ObjectPrinter {
             sb.append('\t');
         }
 
-        sb.append(t.score.isValid ? "Valid   " : "Invalid ");
+//        sb.append(t.score.isValid ? "Valid   " : "Invalid ");
         sb.append('\t');
 
-        sb.append("Total points: ");
+//        sb.append("Total points: ");
         appendDouble(sb, t.score.skillPointTotal);
         sb.append('\t');
 
-        sb.append("By skill total: ");
+//        sb.append("By skill total: ");
         appendArray(sb, t.score.pointsBySkillSum);
         sb.append('\t');
 
-        sb.append("By skill range: ");
+//        sb.append("By skill range: ");
         appendArray(sb, t.score.pointsBySkillRange);
         sb.append('\t');
 
-        sb.append("By skill mean: ");
+//        sb.append("By skill mean: ");
         appendArray(sb, t.score.pointsBySkillMean);
         sb.append('\t');
 
-        sb.append("By skill SD: ");
+//        sb.append("By skill SD: ");
         appendArray(sb, t.score.pointsBySkillSD);
         sb.append('\t');
 
-        sb.append("Mean skill points: ");
+//        sb.append("Mean skill points: ");
         appendDouble(sb, t.score.meanSkillRating);
         sb.append('\t');
 
-        sb.append("Partner preferences met: ");
-        appendDouble(sb, t.score.partnerPreferenceMetPercentage * 100);
+//        sb.append("Partner preferences met: ");
+        appendDouble(sb, t.score.partnerPreferenceMetPercentage, DOUBLE_DECIMAL_PLACES * 2);
         sb.append("%");
         sb.append('\t');
 
-        sb.append("Members with common project: ");
-        appendDouble(sb, t.score.fractionTeamMembersWithCommonProject * 100);
+//        sb.append("Members with common project: ");
+        appendDouble(sb, t.score.fractionTeamMembersWithCommonProject, DOUBLE_DECIMAL_PLACES * 2);
         sb.append("%");
 
         System.out.println(sb.toString());
     }
 
+    private static void appendDouble(StringBuilder sb, double num, int decimalPlaces) {
+        sb.append(String.format("%." + decimalPlaces + "f", num));
+    }
+
     private static void appendDouble(StringBuilder sb, double num) {
-        sb.append(String.format("%." + DOUBLE_DECIMAL_PLACES + "f", num));
+        appendDouble(sb, num, DOUBLE_DECIMAL_PLACES);
     }
 
     public static void appendArray(StringBuilder sb, double[] arr) {
--- src/TeamFormer.java
+++ src/TeamFormer.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.LinkedList;
 
 public class TeamFormer {
 
@@ -13,7 +14,9 @@ public class TeamFormer {
 
     public TeamFormer(String filename) {
 
-
+        double[] skillsWeights = new double[]{0.2, 0.4, 0.6};
+        double[] partnerWeights = new double[]{0.2, 0.5, 1};
+        double[] projectWeights = new double[]{0.2, 0.5, 1};
 
         double colorWeight = 1.1;
         PersonProfile[] profiles;
@@ -28,48 +31,67 @@ public class TeamFormer {
         int numTeams = (int) java.lang.Math.ceil((numPeople / TEAM_SIZE));
         double avgScore = Helper.averageSkillTotal(profiles) * TEAM_SIZE;
 
-
-        graph = new Graph(profiles, SKILLS_WEIGHT, PARTNER_PREFERENCE_WEIGHT, PROJECT_PREFERENCE_WEIGHT);
-        graph.adjacency = Helper.normalize(graph.adjacency);
-
-
         Team[] randomTeams = Helper.randomTeams(profiles, numTeams, TEAM_SIZE);
         TeamSetScore score0 = ResultScorer.scoreTeams(randomTeams, profiles);
+        System.out.printf("The average score of all teams for reference purposes: %f\n\n", avgScore);
+
         System.out.println("Result of random teams:");
         ObjectPrinter.printTeamSetScore(score0);
-        System.out.println("\n\n");
-        Team[] coloredCliques = new ColoredCliques(graph, numTeams, TEAM_SIZE, colorWeight).run(profiles);
-        TeamSetScore score1 = ResultScorer.scoreTeams(coloredCliques, profiles);
-        System.out.println("Result of colored graph:");
-        ObjectPrinter.printTeamSetScore(score1);
-        System.out.println("\n\n");
-        GreedyCliques gc = new GreedyCliques(graph, numTeams, TEAM_SIZE, avgScore);
-        Team[] teamsFromGreedy1 = gc.greedyCliques(profiles);
-        TeamSetScore score2 = ResultScorer.scoreTeams(teamsFromGreedy1, profiles);
-        System.out.println("Result of first greedy implementation:");
-        ObjectPrinter.printTeamSetScore(score2);
-        System.out.println("\n\n");
-        Team[] teamsFromGreedy2 = gc.runV2(0, profiles);
-        TeamSetScore score3 = ResultScorer.scoreTeams(teamsFromGreedy2, profiles);
-        System.out.println("Result of second greedy implementation:");
-        ObjectPrinter.printTeamSetScore(score3);
 
-        System.out.println("\n\n");
+        System.out.println("Sweeping weights...");;
+        graph = new Graph(profiles, SKILLS_WEIGHT, PARTNER_PREFERENCE_WEIGHT, PROJECT_PREFERENCE_WEIGHT);
+        graph.adjacency = Helper.normalize(graph.adjacency);
+        GreedyCliques gc = new GreedyCliques(graph, numTeams, TEAM_SIZE, avgScore);
         Team[] teamsFromAllCliques = gc.allCliques(profiles);
         ResultScorer.scoreTeams(teamsFromAllCliques, profiles);
-        
+        for (double skillsWeight : skillsWeights) {
+            for (double partnerWeight : partnerWeights) {
+                for (double projectWeight : projectWeights) {
+                    System.out.println("Using skills weight: " + skillsWeight + ", partner weight: " + partnerWeight + ", and skill project weight: " + projectWeight);
+                    LinkedList<TeamSetScore> scores = new LinkedList<>();
+
+                    // Generate a graph using the current weights
+                    graph = new Graph(profiles, skillsWeight, partnerWeight, projectWeight);
+                    graph.adjacency = Helper.normalize(graph.adjacency);
+
+                    // First greedy implementation
+                    gc = new GreedyCliques(graph, numTeams, TEAM_SIZE, avgScore);
+                    scores.add(ResultScorer.scoreTeams(gc.greedyCliques(profiles), profiles));
+//                    TeamSetScore score2 = ResultScorer.scoreTeams(teamsFromGreedy1, profiles);
+//                    System.out.println("Result of first greedy implementation:");
+//                    ObjectPrinter.printTeamSetScore(score2);
+
+                    // Second greedy implementation
+                    scores.add(ResultScorer.scoreTeams(gc.runV2(0, profiles), profiles));
+//                    TeamSetScore score3 = ResultScorer.scoreTeams(teamsFromGreedy2, profiles);
+//                    System.out.println("Result of second greedy implementation:");
+//                    ObjectPrinter.printTeamSetScore(score3);
+
+                    scores.add(ResultScorer.scoreTeams(gc.greedyCliques(teamsFromAllCliques, profiles), profiles));
+//		ObjectPrinter.printTeamArray(greedyCliqueTeams);
+//                    TeamSetScore score4 = ResultScorer.scoreTeams(greedyCliqueTeams, profiles);
+//                    System.out.println("Result of runnable allClique implementation:");
+//                    ObjectPrinter.printTeamSetScore(score4);
+
+                    ObjectPrinter.printTeamSetScoreList(scores);
+                }
+            }
+        }
+
+//        Team[] coloredCliques = new ColoredCliques(graph, numTeams, TEAM_SIZE, colorWeight).run(profiles);
+//        TeamSetScore score1 = ResultScorer.scoreTeams(coloredCliques, profiles);
+//        System.out.println("Result of colored graph:");
+//        ObjectPrinter.printTeamSetScore(score1);
+//        System.out.println("\n\n");
+//        System.out.println("\n\n");
+//        GreedyCliques gc = new GreedyCliques(graph, numTeams, TEAM_SIZE, avgScore);
+
         //If you ever have the compute power, uncomment the below to run the bruteforce clique selection
 //		Team[] bruteTeams = bruteCliques(teamsFromAllCliques, scorer, profiles);
 ////		Team[] topTeams = topTeams(teamsFromAllCliques);
 //		ObjectPrinter.printTeamArray(bruteTeams);
 //		System.out.println("we good.");
 
-        Team[] greedyCliqueTeams = gc.greedyCliques(teamsFromAllCliques, profiles);
-//		ObjectPrinter.printTeamArray(greedyCliqueTeams);
-        TeamSetScore score4 = ResultScorer.scoreTeams(greedyCliqueTeams, profiles);
-        System.out.println("Result of runnable allClique implementation:");
-        ObjectPrinter.printTeamSetScore(score4);
-        System.out.printf("The average score of all teams for reference purposes: %f", avgScore);
     }
 
 
